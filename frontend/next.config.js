2ee2f6a8f857d879b0ee2419740d657fd577f5ee
AbdullahBera <kucukabdullahbera@gmail.com>
2024-12-18 23:56:18 -0800

    design

---
+++ frontend/next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Enables React's strict mode for debugging
+};
+
+export default nextConfig;
--- frontend/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true, // Enables React's strict mode for better debugging
-};
-
-export default nextConfig;
